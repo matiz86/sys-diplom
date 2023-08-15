a58905ae422fc8b433ac0b420f03637c6542a0a1
Skopkin Ilya <matiz86@mail.ru>
2023-08-15 20:44:25 +0200

    Add VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,56 @@ provider "yandex" {
   folder_id = var.yc_folder
 }
 
+#Создание vm1, vm2
+
+resource "yandex_compute_instance" "nginxvm1" {
+  name = "nginxvm1"
+  zone = "ru-central1-a"
+
+  resources {
+    cores         = 2
+    core_fraction = 20
+    memory        = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd8o41nbel1uqngk0op2"
+      size        = 10
+      description = "boot disk for nginx_server1"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    nat       = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "nginxvm2" {
+  name = "nginxvm2"
+  zone = "ru-central1-b"
+
+  resources {
+    cores         = 2
+    core_fraction = 20
+    memory        = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd8o41nbel1uqngk0op2"
+      size        = 10
+      description = "boot disk for nginx_server1"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet2.id
+    nat       = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
