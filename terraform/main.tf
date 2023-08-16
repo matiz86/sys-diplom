6b1c3af8ba408943a1b64db1beca05c7cef14754
Skopkin Ilya <matiz86@mail.ru>
2023-08-16 19:21:52 +0200

    Add VM for service

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,3 +68,82 @@ resource "yandex_compute_instance" "nginxvm2" {
     user-data = "${file("./meta.txt")}"
   }
 }
+# Создаем виртуальые машины лоя сервисов
+
+resource "yandex_compute_instance" "zbxserver" {
+  name = "zbxserver"
+  zone = "ru-central1-a"
+  resources {
+    cores         = 2
+    core_fraction = 20
+    memory        = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd8o41nbel1uqngk0op2"
+      size        = 30
+      description = "boot disk for zbxserver"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    nat       = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "kibana" {
+  name = "kibana"
+  zone = "ru-central1-a"
+
+  resources {
+    cores         = 4
+    core_fraction = 20
+    memory        = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd8o41nbel1uqngk0op2"
+      size        = 20
+      description = "boot disk for kibana"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    nat       = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "elastic" {
+  name = "elastic"
+  zone = "ru-central1-a"
+
+  resources {
+    cores         = 4
+    core_fraction = 20
+    memory        = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd8o41nbel1uqngk0op2"
+      size        = 20
+      description = "boot disk for elastic"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    nat       = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
