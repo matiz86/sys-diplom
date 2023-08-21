1ba854534051a56437ef3449f2c6f6dbfb8ed32d
Skopkin Ilya <matiz86@mail.ru>
2023-08-21 18:35:28 +0200

    update

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,17 +1,17 @@
-# Создание сети
-resource "yandex_vpc_network" "network1" {
-  name = "network1"
+# Создание сети и подсети
+resource "yandex_vpc_network" "matiz-sys" {
+  name = "matiz-sys"
 }
 # Создание подсетей
 resource "yandex_vpc_subnet" "subnet1" {
-  name           = "subnet1"
+  name           = "web1"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network1.id
+  network_id     = yandex_vpc_network.matiz-sys.id
   v4_cidr_blocks = ["10.0.1.0/24"]
 }
 resource "yandex_vpc_subnet" "subnet2" {
-  name           = "subnet2"
+  name           = "web2"
   zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network1.id
+  network_id     = yandex_vpc_network.matiz-sys.id
   v4_cidr_blocks = ["10.0.2.0/24"]
 }
