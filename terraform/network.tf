a7cbcaa97c2e08aea707f93c7ba462bc3dfec511
Skopkin Ilya <matiz86@mail.ru>
2023-08-21 20:13:11 +0200

    Add subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,24 +4,48 @@ resource "yandex_vpc_network" "matiz-sys" {
 }
 # Создание подсетей
 resource "yandex_vpc_subnet" "subnet1" {
-  name           = "web1"
+  name           = "web1_private"
   zone           = "ru-central1-a"
+  description    = "subnet for web1_private"
   network_id     = yandex_vpc_network.matiz-sys.id
   v4_cidr_blocks = ["10.0.1.0/24"]
 }
 resource "yandex_vpc_subnet" "subnet2" {
-  name           = "web2"
+  name           = "web2_private"
   zone           = "ru-central1-b"
+  description    = "subnet for web2_private"
   network_id     = yandex_vpc_network.matiz-sys.id
   v4_cidr_blocks = ["10.0.2.0/24"]
 }
 
+resource "yandex_vpc_subnet" "subnet3" {
+  name           = "web3_private"
+  zone           = "ru-central1-b"
+  description    = "subnet for services"
+  network_id     = yandex_vpc_network.matiz-sys.id
+  v4_cidr_blocks = ["10.0.3.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet4" {
+  name           = "pub"
+  zone           = "ru-central1-b"
+  description    = "subnet for bastion"
+  network_id     = yandex_vpc_network.matiz-sys.id
+  v4_cidr_blocks = ["10.0.4.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet5" {
+  name           = "logs"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.matiz-sys.id
+  v4_cidr_blocks = ["10.0.5.0/24"]
+}
 #Создание группы безопасности
 
 resource "yandex_vpc_security_group" "webs" {
   name        = "webs"
   description = "description for my security group"
-  network_id  = yandex_vpc_network.diplom.id
+  network_id  = yandex_vpc_network.matiz-sys.id
 
   ingress {
     protocol       = "ANY"
