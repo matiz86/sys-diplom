eade4a74d87b84854f96bf33e50065aab3d76ebf
Skopkin Ilya <matiz86@mail.ru>
2023-08-21 18:51:12 +0200

    Add tg-groups

--- terraform/webs.tf
+++ terraform/webs.tf
@@ -52,3 +52,19 @@ resource "yandex_compute_instance" "nginxvm2" {
   }
 }
 
+# Target Groups
+
+resource "yandex_alb_target_group" "tg-group" {
+  name = "tg-group"
+
+  target {
+    subnet_id  = yandex_vpc_subnet.subnet1.id
+    ip_address = yandex_compute_instance.nginxvm1.network_interface.0.ip_address
+  }
+
+  target {
+    subnet_id  = yandex_vpc_subnet.subnet2.id
+    ip_address = yandex_compute_instance.nginxvm2.network_interface.0.ip_address
+  }
+
+}
