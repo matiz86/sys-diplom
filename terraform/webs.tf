0e53013bde0dbb42dafc75c9d4b8052e9fc716d9
Skopkin Ilya <matiz86@mail.ru>
2023-08-21 18:57:42 +0200

    Add Backend-Router

--- terraform/webs.tf
+++ terraform/webs.tf
@@ -68,3 +68,51 @@ resource "yandex_alb_target_group" "tg-group" {
   }
 
 }
+
+# Backend_group
+
+resource "yandex_alb_backend_group" "backend-group" {
+  name = "backend-group"
+
+  http_backend {
+    name             = "backend"
+    weight           = 1
+    port             = 80
+    target_group_ids = [yandex_alb_target_group.tg-group.id]
+    load_balancing_config {
+      panic_threshold = 90
+    }
+    healthcheck {
+      timeout             = "5s"
+      interval            = "3s"
+      healthy_threshold   = 10
+      unhealthy_threshold = 15
+      http_healthcheck {
+        path = "/"
+      }
+    }
+  }
+}
+
+# Http-router 
+
+resource "yandex_alb_http_router" "router" {
+  name = "router"
+
+}
+
+resource "yandex_alb_virtual_host" "router-host" {
+  name           = "router-host"
+  http_router_id = yandex_alb_http_router.router.id
+  route {
+    name = "router1"
+    http_route {
+
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.backend-group.id
+        timeout          = "5s"
+      }
+    }
+  }
+}
+
